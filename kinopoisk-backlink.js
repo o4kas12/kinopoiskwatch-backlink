e5574a74f86484f0d589cf33d8bf2743a35ebfa7
Dmitry <59051013+o4kas12@users.noreply.github.com>
2022-01-20 16:39:17 +0300

    работает
    1

---
+++ kinopoisk-backlink.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         kinopoisk-backlink
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+
+const kinopoiskLink = 'https://kinopoisk.ru/';
+const url = window.location.href;
+const splitted = url.split('=');
+const id = splitted[1];
+console.log(id);
+
+//links creation
+let film = kinopoiskLink + "film/" + id;
+console.log("film = " + film);
+
+let logo = document.getElementById("logo");
+logo.href = film;
