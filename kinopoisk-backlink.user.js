c9d60004c015f01ca47eb7ca989baaa803e54db6
Dmitry <59051013+o4kas12@users.noreply.github.com>
2022-01-21 15:10:06 +0300

    Update kinopoisk-backlink.user.js

--- kinopoisk-backlink.user.js
+++ kinopoisk-backlink.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        http://*/*
+// @match        https://kirlovon.dev/Kinopoisk-Watch/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
